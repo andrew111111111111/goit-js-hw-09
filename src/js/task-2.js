8fd7be4252ea928765717072cda1dc69b646bee7
Andrew stecenko <andrew1stecenko@gmail.com>
2026-02-13 14:36:04 +0100

    Add initial HTML structure and implement task scripts for JavaScript exercises

---
+++ src/js/task-2.js
@@ -0,0 +1,31 @@
+// Напиши скрипт підрахунку вартості гравіювання прикрас. Для цього створи функцію calculateEngravingPrice(message, pricePerWord) приймаючу рядок (в рядку будуть тільки слова і прогалини) і ціну гравіювання одного слова, і повертає ціну гравіювання всіх слів в рядку.
+
+const calculateEngravingPrice = function (message, pricePerWord) {
+  const wordsINText = message.split(" ");
+  console.log(wordsINText.length * pricePerWord);
+};
+
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10,
+  ),
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20,
+  ),
+); // 160
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40),
+); // 240
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20),
+); // 120
---
+++ src/js/task-3.js
@@ -0,0 +1,22 @@
+// Напиши функцію findLongestWord(string), яка приймає параметром довільний рядок (в рядку будуть тільки слова і прогалини) і повертає найдовше слово в цьому рядку.
+
+const findLongestWord = function (string) {
+  let biggestWord = "";
+  const stringArray = string.split(" ");
+  for (let i = 0; i < stringArray.length; i++) {
+    const word = stringArray[i];
+    if (word.length > biggestWord.length) {
+      biggestWord = word;
+    }
+  }
+  return biggestWord;
+};
+
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
+
+console.log(findLongestWord("Google do a roll")); // 'Google'
+
+console.log(findLongestWord("May the force be with you")); // 'force'
